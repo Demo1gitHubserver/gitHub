a9b837af6722e09b46470663013bcda4a43525bf
tarena <taerna@tianshihua.local>
2015-07-01 11:02:25 +0800

    init

---
+++ Demo2_gitHubSreve/Demo2_gitHubSreve/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo2_gitHubSreve
+//
+//  Created by tarena on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"add first");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
